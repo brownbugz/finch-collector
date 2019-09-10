d3a41c081f139540701fb6776d0bf6d756dab6ba
sonniakemmer <brownbugz@gmail.com>
2019-09-09 19:07:58 -0700

    adds content

--- main_app/views.py
+++ main_app/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Planner
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Planner Collections</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
 def planners_index(request):
+    planners = Planner.objects.all()
     return render(request, 'planners/index.html', { 'planners': planners })
 
 def planners_detail(request, planner_id):
     planner = Planner.objects.get(id=planner_id)
-    return render(request, 'planners/detail.html')
+    return render(request, 'planners/detail.html', { 'planner': planner })
