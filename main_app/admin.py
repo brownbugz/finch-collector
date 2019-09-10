9e926dcb292e98f0cfd17676d67181efb66c9ef9
sonniakemmer <brownbugz@gmail.com>
2019-09-10 15:25:25 -0700

    add accessory refill date season

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Planner
+from .models import Planner, Accessory
 
 # Register your models here.
 
 admin.site.register(Planner)
+admin.site.register(Accessory)
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Planner
+from .forms import AccessoryForm
 
 class PlannerCreate(CreateView):
     model = Planner
@@ -28,4 +29,14 @@ def planners_index(request):
 
 def planners_detail(request, planner_id):
     planner = Planner.objects.get(id=planner_id)
-    return render(request, 'planners/detail.html', { 'planner': planner })
+    accessory_form = AccessoryForm()
+    return render(request, 'planners/detail.html', 
+      { 'planner': planner, 'accessory_form': accessory_form })
+
+def add_accessory(request, planner_id):
+  form = AccessoryForm(request.POST)
+  if form.is_valid():
+    new_accessory = form.save(commit=False)
+    new_accessory.planner_id = planner_id
+    new_accessory.save()
+  return redirect('detail', planner_id=planner_id)
