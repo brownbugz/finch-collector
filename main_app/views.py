b7057b608dcb59681754cb7618575cc0e8955865
sonniakemmer <brownbugz@gmail.com>
2019-09-17 15:38:41 -0700

    adds @login_require in views.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 import uuid
 import boto3
 from .models import Planner, Gadget, Photo
@@ -10,8 +14,13 @@ S3_BASE_URL = 'https://s3-us-west-1.amazonaws.com/'
 BUCKET = 'catcollector-sjk'
 
 class PlannerCreate(CreateView):
-    model = Planner
-    fields = ['name', 'brand', 'description', 'year']
+  model = Planner
+  fields = ['name', 'brand', 'description', 'year']
+
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+
 
 class PlannerUpdate(UpdateView):
   model = Planner
@@ -21,17 +30,18 @@ class PlannerDelete(DeleteView):
   model = Planner
   success_url = '/planners/'
 
-# Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
+@login_required
 def planners_index(request):
     planners = Planner.objects.all()
     return render(request, 'planners/index.html', { 'planners': planners })
 
+@login_required
 def planners_detail(request, planner_id):
     planner = Planner.objects.get(id=planner_id)
     gadgets_planner_doesnt_have = Gadget.objects.exclude(id__in = planner.gadgets.all().values_list('id'))
@@ -41,6 +51,7 @@ def planners_detail(request, planner_id):
         'gadgets': gadgets_planner_doesnt_have
     })
 
+@login_required
 def add_accessory(request, planner_id):
   form = AccessoryForm(request.POST)
   if form.is_valid():
@@ -49,6 +60,7 @@ def add_accessory(request, planner_id):
     new_accessory.save()
   return redirect('detail', planner_id=planner_id)
 
+@login_required
 def add_photo(request, cat_id):
   photo_file = request.FILES.get('photo-file', None)
   if photo_file:
@@ -63,10 +75,12 @@ def add_photo(request, cat_id):
       print('An error occurred uploading file to S3')
   return redirect('detail', planner_id=planner_id)
 
+@login_required
 def assoc_gadget(request, planner_id, gadget_id):
   Planner.objects.get(id=planner_id).gadgets.add(gadget_id)
   return redirect('detail', planner_id=planner_id)
 
+@login_required
 def unassoc_gadget(request, planner_id, gadget_id):
   Planner.objects.get(id=planner_id).gadgets.remove(gadget_id)
   return redirect('detail', planner_id=planner_id)
