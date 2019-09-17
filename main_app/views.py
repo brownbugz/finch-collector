5af98f8c71faaaf9d0e7b9fbb143ec5c3d4f7eff
sonniakemmer <brownbugz@gmail.com>
2019-09-17 15:42:23 -0700

    adds signup function in views.py and updated manage.py

--- main_app/views.py
+++ main_app/views.py
@@ -102,3 +102,21 @@ class GadgetUpdate(UpdateView):
 class GadgetDelete(DeleteView):
   model = Gadget
   success_url = '/gadgets/'
+
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {
+    'form': form,
+    'error_message': error_message
+  }
+  return render(request, 'registration/signup.html', context)
